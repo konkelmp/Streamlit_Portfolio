f00b096b0b58afc207db64a4c74d02c940163f9c
Preston Konkel <80935110+konkelmp@users.noreply.github.com>
2025-11-02 22:35:59 -0700

    Fix markdown formatting in app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_page_config(
 st.title(" Welcome to My Streamlit Site")
 st.markdown(
     """
-    This is a Streamlit Dashboard app for displaying regional wildfire and air quality data:
+    This is a Streamlit Dashboard app for displaying regional wildfire and air quality data
     - Preston Konkel
 
     Use the left sidebar switch regions and data time ranges.
