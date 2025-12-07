981f500921390fdea9d0a4b37f35a7e645e6552e
Preston Konkel <80935110+konkelmp@users.noreply.github.com>
2025-12-06 21:07:24 -0700

    Update Regional_Fires_Dashboard.py

--- pages/Regional_Fires_Dashboard.py
+++ pages/Regional_Fires_Dashboard.py
@@ -62,7 +62,7 @@ if (confidence != ""):
 
 ###########################################
 
-col1, col2 = st.columns([2, 1])
+col1, col2, col3 = st.columns([2, 1, 1])
 
 # Folium map
 with col1:
@@ -94,18 +94,19 @@ with col2:
         st.line_chart(daily_counts.set_index('acq_date'))
     else:
         st.info("No data available for this selection.")
+
+with col2:
+    #  KPI Metrics
+    st.subheader("📊 Key Metrics")
+    
+    st.metric("Total Fires", len(filtered_df))
+    avg_frp = round(filtered_df['frp'].mean(), 2)
+    st.metric("Average Fire Radiative Power (FRP) in MegaWatts", avg_frp)
+    
+    day_count = (filtered_df['daynight'] == "D").sum()
+    night_count = (filtered_df['daynight'] == "N").sum()
+    st.metric("Detection Day or Night", f"{day_count} / {night_count}")
+    
+    st.metric("Top Reporting Satellite", filtered_df['satellite'].mode()[0])
         
 ###########################################
-
-#  KPI Metrics
-st.subheader("📊 Key Metrics")
-
-st.metric("Total Fires", len(filtered_df))
-avg_frp = round(filtered_df['frp'].mean(), 2)
-st.metric("Average Fire Radiative Power (FRP) in MegaWatts", avg_frp)
-
-day_count = (filtered_df['daynight'] == "D").sum()
-night_count = (filtered_df['daynight'] == "N").sum()
-st.metric("Detection Day or Night", f"{day_count} / {night_count}")
-
-st.metric("Top Reporting Satellite", filtered_df['satellite'].mode()[0])
