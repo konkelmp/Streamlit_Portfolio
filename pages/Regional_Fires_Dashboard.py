da2353b7cfe9346fb9bfe34c672c6399c0cbe0a6
Preston Konkel <80935110+konkelmp@users.noreply.github.com>
2025-12-06 20:09:40 -0700

    Update Regional_Fires_Dashboard.py

--- pages/Regional_Fires_Dashboard.py
+++ pages/Regional_Fires_Dashboard.py
@@ -62,7 +62,7 @@ filtered_df = filtered_df[filtered_df['confidence'] == confidence]
 ###########################################
 
 #  Wildfire Choropleth Map
-st.subheader(f"🔥 Wildfires in {region}")
+st.subheader(f"🔥 Fire in {region_select}")
 fire_map = folium.Map(location=[(lat_min + lat_max)/2, (lon_min + lon_max)/2], zoom_start=3)
 marker_cluster = MarkerCluster().add_to(fire_map)
 
